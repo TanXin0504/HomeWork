9d43fd881b504e94c72f17c19d68e62167a98ee6
tanxin <1456758912@qq.com>
2020-07-25 09:24:46 +0800

    一键同步

--- src/main/java/com/accp/action/GoodsAction.java
+++ src/main/java/com/accp/action/GoodsAction.java
@@ -28,7 +28,7 @@ public class GoodsAction {
 	public PageInfo<Goods> queryAll(@PathVariable Integer p,@PathVariable Integer s){
 		System.out.println(1);
 		System.out.println(2);
-		System.out.println(3);
+		System.out.println(4);
 		return goodsBiz.queryAll(p, s);
 	}
 	
